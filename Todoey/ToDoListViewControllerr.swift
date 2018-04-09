0cdbe9d4ae6d32518658160b3838421de227cf8f
Your Name <you@example.com>
2018-04-09 12:08:32 +0300

    Add alert

--- Todoey/ToDoListViewControllerr.swift
+++ Todoey/ToDoListViewControllerr.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController  {
 
-    let itemArray = ["Food", "Sport", "Game"]
+    var itemArray = ["Food", "Sport", "Game"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,6 +46,31 @@ class ToDoListViewController: UITableViewController  {
         
     }
     
+    //MARK: Add new item section
+    
+    @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoye Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+            
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            
+        }
+        
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+
+        
+    }
     
     
 }
